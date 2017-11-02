bd0595d45973b208defaa64d95a2390aa55cb01b
yatingkun <652111934@qq.com>
2017-11-02 22:53:38 +0800

    重新开始=重新加载页面

--- js/look.js
+++ js/look.js
@@ -1,9 +1,6 @@
 function reset(){
     
     if(confirm("是否重新开始?")){
-       var li= document.getElementsByClassName("card");
-       for (var k = 0; k < li.length; k++) {
-           li[k].className="card";
-       }      
+      window.location.href="index.html";            
     }
 }
