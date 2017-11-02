f73499d6d93039bba596e3b317478196a72781c5
yatingkun <652111934@qq.com>
2017-11-02 18:00:47 +0800

    还差如何在匹配图片成功后取消事件监听，主体流程基本做好了

--- js/app.js
+++ js/app.js
@@ -25,16 +25,68 @@ function shuffle(array) {
     return array;
 }
 window.onload=function(){
-	var card = document.getElementsByClassName('card');
-	for (var i = 0; i < card.length; i++) {
-		(function(i){
-		card[i].addEventListener('click',function(e){
-		card[i].className="card open show";
+	var _testcards=[];//用来存放互相匹配的卡片，长度最多为2。
+    var id=[];//用来记录卡片的索引值，同时在图片不匹配时能使用
+    var match_cards=[];//用来存放匹配成功的卡片，长度为16时游戏结束。
+	var card = document.getElementsByClassName('card');//获取有图片的li元素节点，返回一个数组对象
+    /*给有图片的li元素节点绑定事件监听*/
+	for (var i = 0; i < card.length; i++) {  
+		(function(i){			
+		card[i].addEventListener('click',function(e){ 
+         /*让卡片在每一次比较中只能点击一次*/
+         id.push(i);
+         if(id[0]!=id[1]){
+            _testcards.push(card[i]);
+            card[i].className="card show open";
+         }
+         else{
+            id.pop();
+         }
+         /*当点击两张不同的卡片后，在_testcards数组中将图片比较，相同则改变两张图片的样式，存入match_cards数组，并取消_testcards数组中Li的时件监听，
+         同时将_testcards数组清零，好做下一次的比较。*/
+         while(_testcards.length==2){
+         	if(_testcards[0].getElementsByTagName("i")[0].className ==_testcards[1].getElementsByTagName("i")[0].className){ 
+                    for (var j = 0; j < _testcards.length; j++) 
+                        {
+                            _testcards[j].className="card match";
+                            match_cards.push(_testcards[j]);
+                           // _testcards[j].removeEvenListener=('click',e,false);匿名函数无法取消监听绑定，正在想办法。
+                        } 
+
+                            id.length=0;
+         	    }
+                /*当两张卡片不同，就将卡片恢复原样，由于视觉效果，必须使用setTimeout来改变图片的样式，因为我设置有 _testcards的清零，
+                所有无法再使用_testcards数组中索引来改变图片，却刚好能将id数组中的索引用过来。比较后id数组清零。*/
+         	else{
+                    card[id[0]].className="card show error";
+                    card[id[1]].className="card show error";                  
+                    setTimeout(function(){
+                        card[id[0]].className="card ";
+                        card[id[1]].className="card ";
+                        id.length=0;
+                        },1000);
+         	    }
+         	_testcards.length=0;
+            /*当匹配成功的的卡片数组长度为16时游戏完成*/
+            if(match_cards.length==16){
+                setTimeout(function(){
+                   alert("恭喜你完成游戏！");
+                   for (var i = 0; i < card.length; i++) {
+                       card[i].className="card";
+                   }
+                   match_cards.length=0;
+                    },500);
+            }
+         }	
+
 	    });
+
 	  })(i)
+	   
 	}
 	
 }
+
 
 
 /*
