03a563815dcc9447c5a71c0fdbe6c107403d2394
yatingkun <652111934@qq.com>
2017-11-04 21:00:00 +0800

    设置了星级随步数的增加而改变，接下来会加入时间条件，让步数与时间共同决定星级

--- js/app.js
+++ js/app.js
@@ -22,11 +22,20 @@ var cards_style = ['fa-diamond','fa-diamond',
 'fa-bicycle','fa-bicycle',
 'fa-bomb','fa-bomb'];
 var str = '';
+var start='';       //重置时访问。
+var counter=0;      //记录步数，每翻开两次卡片算一步。
 shuffle(cards_style);
 for (var i = 0; i<cards_style.length;i++){
     str += '<li class = \"card\"><i class=\"fa '+cards_style[i]+'\"></i></li>\n';
 }
 document.getElementsByClassName("deck")[0].innerHTML = str;
+if(counter==0){
+    for (var i = 0; i < 3; i++) {
+      start += '<li><i class=\"fa fa-star\"></i></li>\n';
+ }
+ document.getElementsByClassName("stars")[0].innerHTML = start;
+ document.getElementsByClassName("moves")[0].innerHTML = i;
+ }
 /******************************游戏主要流程***********************************************/
 	var _testcards=[];     //用来存放互相匹配的卡片，长度最多为2。
 
@@ -35,83 +44,100 @@ document.getElementsByClassName("deck")[0].innerHTML = str;
     var match_cards=[];     //用来存放匹配成功的卡片，长度为16时游戏结束。
 
 	var card = document.getElementsByClassName('card');        //获取有图片的li元素节点，返回一个数组对象。
-
-    var counter=0;      //记录步数，每翻开两次卡片算一步。
-
-	 /*for (var i = 0; i < card.length; i++) {        /*要想成功取消事件监听，handler函数必须前后一致，所以取消这种绑定方法。
-     
-		(function(i){*/	
 		
          /*让卡片在每一次比较中只记录一次*/
-         var  handler=function(){
+        var  handler=function(){
             var i = this.dataset.id;   //传入索引值i。
             car_id.push(i);
             _testcards.push(card[i]);
             card[i].className="card show open";
             if(car_id[0]==car_id[1]){
                 car_id.splice(1,1);
-                 _testcards.splice(1,1);
+                _testcards.splice(1,1);
             }
          /*当点击两张不同的卡片后，在_testcards数组中将图片比较，相同则改变两张图片的样式，存入match_cards数组，并取消_testcards数组中Li的时件监听，
          同时将_testcards数组清零，好做下一次的比较。*/
-         else
-         {   
+            else{
 
-         	if(_testcards[0].getElementsByTagName("i")[0].className ==_testcards[1].getElementsByTagName("i")[0].className){
+         	       if(_testcards[0].getElementsByTagName("i")[0].className ==_testcards[1].getElementsByTagName("i")[0].className){
                 
-                    for (var j = 0; j < _testcards.length; j++) {
+                        for (var j = 0; j < _testcards.length; j++) {
             
-                            (function(j){
-                                _testcards[j].className="card match";
-                                match_cards.push(_testcards[j]);
-                                _testcards[j].removeEventListener('click',handler,false);
-                            })(j)
+                                (function(j){
+                                    _testcards[j].className="card match";
+                                    match_cards.push(_testcards[j]);
+                                    _testcards[j].removeEventListener('click',handler,false);
+                                })(j)
                           
-                    }     
+                        }     
                             _testcards.length=0;
                             car_id.length=0;                    
-         	}
+         	        }
                 /*当两张卡片不同，就将卡片恢复原样，由于视觉效果使用setTimeout来改变图片的样式*/
-         	else{ 
-                    _testcards[0].className="card show error";
-                    _testcards[1].className="card show error";                  
-                    setTimeout(function(){
-                        _testcards[0].className="card ";
-                        _testcards[1].className="card ";
-                        _testcards.length=0;
-                        car_id.length=0;
-                        },300);
-         	    }
+         	        else{
+
+                        _testcards[0].className="card show error";
+                        _testcards[1].className="card show error";                  
+                        setTimeout(function(){
+                            _testcards[0].className="card ";
+                            _testcards[1].className="card ";
+                            _testcards.length=0;
+                            car_id.length=0;
+                            },500);
+         	        }
             /*当匹配成功的的卡片数组长度为16时游戏完成*/
-            if(match_cards.length==16){
+                    if(match_cards.length==16){
                 
-                setTimeout(function(){
-                    reset("恭喜你完成游戏！是否重新开始?\n共用步数："+counter);
-                    match_cards.length=0;                
-                },100);
-            }
+                        setTimeout(function(){
+                        reset("恭喜你完成游戏！是否重新开始?\n共用步数："+counter);
+                        match_cards.length=0;                
+                        },100);
+                        }
             ++counter;
             document.getElementById("steps").innerHTML=counter;
-         }	
-
+            setstatr(counter);
+            if(counter>32){reset("很遗憾！游戏失败！请在32步以内完成！")}  //游戏需要在32步以内完成。
+            }	
+           
 	    }
          /*给有图片的li元素节点绑定事件监听*/
-        for (var i = 0; i < card.length; i++) { 
-            card[i].dataset['id'] = i;       //给handler函数传入参数i,既卡片索引。
-            card[i].addEventListener('click',handler,false);
-	 /* })(i)*/
-	    }
-	//}
-/***************重新开始***********************************/
+for (var i = 0; i < card.length; i++) { 
+
+    card[i].dataset['id'] = i;       //给handler函数传入参数i,既卡片索引。
+    card[i].addEventListener('click',handler,false);
+ }
+
+ /**********设置星级,16到24以内完成获得二星，24到32步一星********************/
+ function setstatr(n){
+
+     if(n>=16){
+          var start='';
+          for (var i = 0; i < 2; i++) {
+          start += '<li><i class=\"fa fa-star\"></i></li>\n';
+          } 
+        if(n>=24){
+            var i=1;
+            var start = '<li><i class=\"fa fa-star\"></i></li>\n';
+        }
+    document.getElementsByClassName("stars")[0].innerHTML = start;
+    document.getElementsByClassName("moves")[0].innerHTML = i;
+     }
+ }
+ 
+/***************重新开始***********************/
 function reset(mesege){ 
        
     if(confirm(mesege)){
+        
         for (var i = 0; i < card.length; i++) {
             card[i].className="card";
             card[i].dataset['id'] = i;       //给handler函数传入参数i,既卡片索引。
             card[i].addEventListener('click',handler,false);
             document.getElementById("steps").innerHTML="0";
             counter=0;
+            document.getElementsByClassName("stars")[0].innerHTML = start;
+            document.getElementsByClassName("moves")[0].innerHTML = 3;
         }          
     }
 }
+/***************计时器**********************/
